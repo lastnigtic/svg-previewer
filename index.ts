f2cc66849c9469c3fe27379ab7c69e68e62f4d28
lastnigtic <1113594660@qq.com>
2022-05-24 09:33:56 +0800

    feat: support `directory` in arguments

--- index.ts
+++ index.ts
@@ -4,9 +4,8 @@ import { program } from 'commander';
 import { startServer } from './server';
 
 program
-	.option('-d, --directory <string>', 'relative directory to preview svg files.', '.')
-	.option('-o, --open <boolean>', 'open page in browser', true);
-
-program.parse();
-
-startServer({ ...program.opts() });
+	.option('-d, --directory <string>', 'relative directory to preview svg files.')
+	.option('-o, --open <boolean>', 'open page in browser', true)
+	.argument('[directory]', 'relative directory to preview svg files.')
+	.action((directory, options) => startServer({ directory, ...options }))
+	.parse();
